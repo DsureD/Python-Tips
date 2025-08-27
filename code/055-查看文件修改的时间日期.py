bf719ed72e85ebca864ef5ac82b59db6e3171d90
DsureD <152784340+DsureD@users.noreply.github.com>
2025-08-27 16:35:07 +0800

    Update 055-查看文件修改的时间日期.py

--- code/055-查看文件修改的时间日期.py
+++ code/055-查看文件修改的时间日期.py
@@ -11,3 +11,14 @@ readable_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(mtime))
 
 print("最后修改时间戳:", mtime)
 print("最后修改时间:", readable_time)
+
+# 更简单的一种方法 --------------------------------------
+'''
+from pathlib import Path
+import time
+
+file_path = Path("example.txt")
+
+mtime = file_path.stat().st_mtime
+print("最后修改时间:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(mtime)))
+'''
